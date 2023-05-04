727bd6dc7d37b83a4a6b62f65d548ee9533755f3
sohag292 <sohagshah571@gmail.com>
2023-05-04 18:26:28 +0600

    chiefDetails data successfully create

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const app = express()
+const app = express();
+const cors = require('cors');
 const port = process.env.PORT||5000;
+
+const chiefprofile = require('./chiefData/chiefProfile.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('IndianRecipe is running')
   })
+
+app.get('/chieprofile', (req, res) => {
+    res.send(chiefprofile)
+})
 
 app.listen(port, () => {
     console.log(`IndianRecipe API is running port ${port}`)
